0184be4b316f765026e50649088aa04121086460
huascupi <huascupiestefany@gmail.com>
2016-08-09 19:38:30 -0500

    Agregamos presentacion y comportamiento

--- hola-php.php
+++ hola-php.php
@@ -1,7 +1,38 @@
 <!DOCTYPE HTML>
 <html lang="es">
-     <body>
-        <h1>hola php</h1>
+    <head>
+    <title>HOLA PHP</title>
+         <style type="text/css">
+            /*presentacion*/
+            h1{color:peru;}
+            p{color:#adcbf7;}
+            li:first-letter{font-size: 2em;}
+        </style> 
+          <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    </head>
+     <body>
+             <h1>hola PHP </h1>
+             <p> dias de la semana</p>
+             <a class="btn btn-primary" href="http://www.google.com">ir a google </a>
+             <ol>
+                 <li>Lunes</li>
+                 <li>Martes</li>
+                 <li>Miercoles</li>
+                 <li>Jueves</li>
+                 <li>Viernes</li>
+                 <li>Sabado</li>
+                 <li>Domingo</li>
+            </ol>
+        <script type="text/javascript">
+            alert("hola:"+prompt("estefany"));
+        </script>
+            
      </body>
-   </html>
+</html>
