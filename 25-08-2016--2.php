a9fb7e45758f2285aee25df21a3def0d070f4fd3
huascupi <huascupiestefany@gmail.com>
2016-08-25 18:07:24 -0500

    programacion con tilde

---
+++ 25-08-2016--2.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+   <?php
+    /*Tablas de codigos(encoding)*/
+       echo"<P>Peru</P>">
+   ?>
+</body>
+</html>
---
+++ 25-08-2016.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+   <?php
+    /*Tablas de codigos(encoding)*/
+       echo"<P>Per&uacute</P>";
+   ?>
+</body>
+</html>
